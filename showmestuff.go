34b1e08e968242304e3462c661e7d76db7b1bfb5
vaibhav.bansal <vaibhav.bansal@epicor.com>
2021-07-29 15:30:28 +0530

    Use the bufio to get the input from standard input screen. - the ReadString also returns the delimiter as part of the read string.

--- showmestuff.go
+++ showmestuff.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"runtime"
 )
 
 func main() {
-	fmt.Printf("hello we are running go: %v", runtime.Version())
+	fmt.Printf("hello we are running go: %v\n", runtime.Version())
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("whats ur name")
+	//provide \n to indicate the delimeter, till \n the reader going to use
+	//IMP: text will also contain the last \n, so when we just print it, we will have a line at the end of the text.
+	text, _ := reader.ReadString('\n')
+	fmt.Print(text)
+
 }
